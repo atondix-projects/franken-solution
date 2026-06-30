784deb60723670b98520a25517f7ea387d2a8fb8
Atondix Cockpit <cockpit@atondix.de>
2026-06-30 13:41:55 +0000

    Update Atondix site content

--- src/content/impressum.ts
+++ src/content/impressum.ts
@@ -2,16 +2,7 @@ export const impressumContent = {
   vorzeile: "Rechtliches",
   überschrift: "Impressum",
   beschreibung: "Angaben gemäß § 5 TMG",
-  hinweise: [
-    "Unternehmensname: Franken Solution",
-    "Adresse: Winzelbürgstr. 9, 90491 Nürnberg, Deutschland",
-    "Telefon: 0911 95898731",
-    "E-Mail: kontakt@franken-solution.de",
-    "Vertreten durch: Markus Maenner, Sergej Stasenko (Geschäftsführer)",
-    "Handelsregister: Placeholder: HRB-Nummer",
-    "Registergericht: Placeholder: Amtsgericht",
-    "Umsatzsteuer-ID: Placeholder: USt-IdNr. gemäß § 27 a UStG",
-  ],
+  hinweise: ["Franken-Solution Maenner und Stasenko GbR", "Winzelbürgstr.9", "90491 Nürnberg", "Telefon: +49 (0)911 95898731", "Telefax: +49 (0)911 96844888", "E-Mail: kontakt@franken-solution.de", "Vertreten durch:", "Markus Maenner", "Sergej Stasenko", "Umsatzsteuer-ID:", "Umsatzsteuer-Identifikationsnummer nach §27a Umsatzsteuergesetz:", "Umsatzsteuer-ID: DE324774956"],
   nächsterSchritt: { beschriftung: "Zur Startseite", verlinkung: "/" },
 } as const;
 
